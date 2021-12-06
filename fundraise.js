6f1042f9032336d0c1607b53f7f0ba333eec2fcf
Ethan Kies <ethankies27@gmail.com>
2021-12-05 19:19:42 -0500

    add counter for fundraise progress

---
+++ fundraise.js
@@ -0,0 +1,16 @@
+$(function() {
+    function count($this){
+        var current = parseInt($this.html(), 10);
+        $this.html(current+=1);
+        if(current !== $this.data('count')){
+            setTimeout(function(){count($this)}, 50);
+        }
+    }        
+  $("span#num").each(function() {
+      $(this).data('count', parseInt($(this).html(), 10));
+      $(this).html('0');
+      count($(this));
+  });
+});
+
+
